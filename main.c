2f5efede9aa19b33598659d60ee1813e95aedd10
JosephDonoso <86876275+JosephDonoso@users.noreply.github.com>
2022-04-29 17:52:29 -0400

    Modificando función propuesta
    Eliminando línea de mi función propuesta. Era una variable definida que no estaba siendo ocupada.

--- main.c
+++ main.c
@@ -199,7 +199,6 @@ void importar(Stock* almacen){
         }
     }
     char linea[1024];
-    char nombreLista[31];
     char* aux;
     size_t cantGeneros;
     while (fgets (linea, 1023, fp) != NULL) { 
