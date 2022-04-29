232a24b7fd51c0b709f54b7c572d6c426adb79ff
JosephDonoso <86876275+JosephDonoso@users.noreply.github.com>
2022-04-29 17:44:49 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <conio.h>
+#include <conio.h> //En teoria se puede ya eliminar esta libreria
 #include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
@@ -11,21 +11,33 @@ typedef struct{
     char nombre[31];
     List* productos;
     size_t cantProductos;
-    size_t cantTotalProductos;
 }Carrito;  
+
+//Eliminar esta struct, para solo inicializar un HashMap* MapCarritos en el main
+typedef struct{
+    HashMap* mapCarritos;
+    size_t cantCarritos;
+} Carritos;
+
+/*  PROPUESTA DE STRUCT
+    typedef struct{
+        Producto* referencia; //Como idea es que el producto que se agrega al carrito tenga un puntero de referencia al producto real,
+        size_t cantidad;      //para conseguir todos los datos y facilitar posteriormente funciones como "eliminar del carrito"
+    }ProductoCarrito;
+*/
+typedef struct{
+    char nombre[51];
+    size_t cantidad;
+    size_t valor;
+}ProductoCarrito;
 
 typedef struct{
     char nombre[51];
-    char marca[21];
-    char tipo[21];
+    char marca[31];
+    char tipo[31];
     size_t stock;
     size_t valor;
 } Producto;
-
-typedef struct{
-    size_t cantidad;
-    Producto* referencia;
-}ProductoCarrito;
 
 typedef struct{
     HashMap* tipo;
@@ -83,9 +95,7 @@ const char *get_csv_field (char * tmp, int k) {
 
 bool esNumero(char *caracter) {
     int cont;
-    
     if( !strcmp(caracter, "0") ) return false;
-
     for (cont = 0; cont < strlen(caracter); cont++) {
         if (isdigit(caracter[cont]) != true) {
             return false;
@@ -355,6 +365,7 @@ void menuAgregarProducto( Stock* almacen ){
     }
     printf("\n================= VOLVIENDO AL MENU ==================\n");
     system("pause");
+    return;
 }
 
 void buscarTipo(HashMap* MapTipo){
@@ -369,7 +380,7 @@ void buscarTipo(HashMap* MapTipo){
 
     char tipoDeProducto[31];
     system("cls");
-    printf("\n================================== BUSCANDO PRODUCTOS POR TIPO ==================================\n");
+    printf("\n========================= BUSCANDO PRODUCTOS POR TIPO ========================\n");
     printf("\nIngrese el tipo de producto: ");
     fflush(stdin);
     gets(tipoDeProducto);
@@ -379,387 +390,36 @@ void buscarTipo(HashMap* MapTipo){
     Producto* prdctoAux ;
 
     if( (aux = searchMap( MapTipo, tipoDeProducto)) ){
-        printf("\n==================== PRODUCTO ========================= MARCA =========== CANTIDAD ==== PRECIO ==\n");
+        printf("\n==================== PRODUCTO ====================");
+        printf("== CANTIDAD ==");
+        printf("== PRECIO ====\n");
         mapaAux = (HashMap *)aux->value;
         aux = firstMap(mapaAux);
 
         while(aux){
 
             prdctoAux = (Producto*) aux->value;
-            printf("\n%50s", prdctoAux->nombre);
-            printf("%20s", prdctoAux->marca);
-            printf("%12i", prdctoAux->stock);
+            printf("\n%50s    ", prdctoAux->nombre);
+            printf("%7i", prdctoAux->stock);
             printf("%12i", prdctoAux->valor);
             aux = nextMap(mapaAux);
 
         }
-        printf("\n\n======================================= BUSQUEDA EXITOSA ========================================\n");
+        printf("\n\n============================== BUSQUEDA EXITOSA ==============================\n");
     }
     else{
-        printf("\n                               NO EXISTEN PRODUCTOS DE ESE TIPO\n");
-        
-        printf("\n====================================== VOLVIENDO AL MENU ========================================\n");
-    }
-
-    system("pause");
-}
-
-void buscarMarca(HashMap* MapMarca){
-    if( MapMarca->size == 0 ){
-        system("cls");
-        printf("\n========================= BUSCANDO PRODUCTOS POR MARCA =======================\n");
-        printf("\n                 NO HAY PRODUCTOS EN EL ALMACEN PARA MOSTRAR\n");
+        printf("\n                       NO EXISTEN PRODUCTOS DE ESE TIPO\n");
         printf("\n============================= VOLVIENDO AL MENU ==============================\n");
-        system("pause");
-        return;
     }
 
-    char marcaDelProducto[31];
-    system("cls");
-    printf("\n================================== BUSCANDO PRODUCTOS POR MARCA =================================\n");
-    printf("\nIngrese la marca de producto: ");
-    fflush(stdin);
-    gets(marcaDelProducto);
-
-    HashMap* mapaAux ;
-    Pair* aux ;
-    Producto* prdctoAux ;
-
-    if( (aux = searchMap( MapMarca, marcaDelProducto)) ){
-        printf("\n==================== PRODUCTO ========================= TIPO ============ CANTIDAD ==== PRECIO ==\n");
-        mapaAux = (HashMap *)aux->value;
-        aux = firstMap(mapaAux);
-
-        while(aux){
-
-            prdctoAux = (Producto*) aux->value;
-            printf("\n%50s", prdctoAux->nombre);
-            printf("%20s", prdctoAux->tipo);
-            printf("%12i", prdctoAux->stock);
-            printf("%12i", prdctoAux->valor);
-            aux = nextMap(mapaAux);
-
-        }
-        printf("\n\n======================================= BUSQUEDA EXITOSA ========================================\n");
-    }
-    else{
-        printf("\n                               NO EXISTEN PRODUCTOS DE ESA MARCA\n");
-        printf("\n====================================== VOLVIENDO AL MENU ========================================\n");
-    }
-
-    system("pause");
-}
-
-void buscarNombre(HashMap* MapNombre){
-    if( MapNombre->size == 0 ){
-        system("cls");
-        printf("\n=============== BUSCANDO PRODUCTO POR NOMBRE =============\n");
-        printf("\n         NO HAY PRODUCTOS EN EL ALMACEN PARA MOSTRAR\n");
-        printf("\n=================== VOLVIENDO AL MENU ====================\n");
-        system("pause");
-        return;
-    }
-
-    char nombreDelProducto[31];
-    system("cls");
-    printf("\n=============== BUSCANDO PRODUCTO POR NOMBRE =============\n");
-    printf("\nIngrese el nombre del producto: ");
-    fflush(stdin);
-    gets(nombreDelProducto);
-
-    Pair* aux ;
-    Producto* prdctoAux ;
-
-    if( (aux = searchMap( MapNombre, nombreDelProducto)) ){
-        prdctoAux = (Producto*) aux->value;
-        
-        printf("\nNOMBRE: %s    ", prdctoAux->nombre);
-        printf("\nMARCA:  %s    ", prdctoAux->marca);
-        printf("\nTIPO:   %s    ", prdctoAux->tipo);
-        printf("\nSTOCK: %6i", prdctoAux->stock);
-        printf("\nPRECIO:%6i", prdctoAux->valor);
-        printf("\n\n==================== BUSQUEDA EXITOSA ====================\n");
-    }
-    else{
-        printf("\n           NO EXISTE UN PRODUCTO CON ESE NOMBRE\n");
-        printf("\n=================== VOLVIENDO AL MENU ====================\n");
-    }
-
-    system("pause");
-}
-
-void mostrarProductos(HashMap* MapNombre){
-    system("cls");
-    if( MapNombre->size == 0 ){
-        printf("\n======================= MOSTRANDO TODOS LOS PRODUCTOS ========================\n");
-        printf("\n                 NO HAY PRODUCTOS EN EL ALMACEN PARA MOSTRAR\n");
-        printf("\n============================= VOLVIENDO AL MENU ==============================\n");
-        system("pause");
-        return;
-    }
-    printf("\n========================================== MOSTRANDO TODOS LOS PRODUCTOS ============================================\n");
-    printf("\n==================== PRODUCTO ========================= MARCA ============= TIPO ============ CANTIDAD ==== PRECIO ==\n");
-    Pair* aux;
-    Producto* prdctoAux;
-    aux = firstMap(MapNombre);
-    while(aux){
-        prdctoAux = (Producto*) aux->value;
-        printf("\n%50s", prdctoAux->nombre);
-        printf("%20s", prdctoAux->marca);
-        printf("%20s", prdctoAux->tipo);
-        printf("%12i", prdctoAux->stock);
-        printf("%12i", prdctoAux->valor);
-
-        aux = nextMap(MapNombre);
-    }
-    printf("\n\n================================================ VOLVIENDO AL MENU ==================================================\n");
-    system("pause");
-}
-
-Carrito* createCarrito( char* nombre ){
-    Carrito* carrito = (Carrito*) malloc( sizeof(Carrito) );
-    strcpy(carrito->nombre , nombre);
-    carrito->cantProductos = 0;
-    carrito->productos = createList();
-    carrito->cantTotalProductos = 0;
-    return carrito;
-}
-
-void agregarProductoCarrito(Stock* almacen, HashMap* MapCarritos){
-     system("cls");
-    if( almacen->nombre->size == 0 ){
-        printf("\n========== AGREGAR PRODUCTO AL CARRITO ==========\n");
-        printf("\n         NO HAY PRODUCTOS EN EL ALMACEN\n");
-        printf("\n============== VOLVIENDO AL MENU ================\n");
-        system("pause");
-        return;
-    }
-
-    char reintento[2];
-    char nombreAux[51];
-    while( true ){
-        system("cls");
-        printf("\n========== AGREGAR PRODUCTO AL CARRITO ==========\n");
-        printf("\nNombre del producto: ");
-        fflush(stdin);
-        gets(nombreAux);
-        Pair* aux;
-        Producto* productoAux;
-        Carrito* carritoAux;
-        if( aux = searchMap( almacen->nombre , nombreAux ) ){
-            productoAux = ( Producto* ) aux->value;
-            printf("\nNombre del carrito: ");
-            fflush(stdin);
-            gets(nombreAux);
-
-            if( !(aux = searchMap( MapCarritos, nombreAux )) ){
-                Carrito* nuevoCarrito = createCarrito(nombreAux);
-                insertMap(MapCarritos, nuevoCarrito->nombre, nuevoCarrito);
-                aux = searchMap( MapCarritos, nombreAux);
-                printf("\nNUEVO CARRITO CREADO\n");
-            }
-
-            carritoAux = ( Carrito* ) aux->value;
-            ProductoCarrito* prdctoCarrito = (ProductoCarrito*) malloc( sizeof(ProductoCarrito) );
-            prdctoCarrito->referencia = productoAux;
-
-            char numeroAux[11];
-            do{
-                printf("\nIngrese la cantidad de productos que agregara: ");
-                fflush(stdin);
-                scanf("%s", numeroAux);
-
-                while(!esNumero(numeroAux)){
-                    printf("Debe ser un numero: ");
-                    fflush(stdin);
-                    scanf("%s", numeroAux);
-                }
-
-                prdctoCarrito->cantidad = atoi(numeroAux);
-                if(prdctoCarrito->cantidad > productoAux->stock){
-                    printf("\nDEBE SER UNA CANTIDAD MENOR AL STOCK DEL PRODUCTO\n");
-                }
-
-            } while ( prdctoCarrito->cantidad > productoAux->stock );
-
-            productoAux->stock -= prdctoCarrito->cantidad;
-            if( productoAux->stock == 0){ 
-                eraseMap(almacen->nombre, prdctoCarrito->referencia->nombre);
-                aux = searchMap(almacen->tipo, prdctoCarrito->referencia->tipo);
-                eraseMap (aux->value, prdctoCarrito->referencia->nombre);
-                aux = searchMap(almacen->marca, prdctoCarrito->referencia->marca);
-                eraseMap (aux->value, prdctoCarrito->referencia->nombre);
-            }
-
-            pushBack( carritoAux->productos, prdctoCarrito);
-            carritoAux->cantProductos += 1;
-            carritoAux->cantTotalProductos += prdctoCarrito->cantidad;
-            printf("\nPRODUCTO AGREGADO CON EXITO\n");
-        }
-        else{
-            printf("\nPRODUCTO NO ENCONTRADO\n");
-        }
-        
-        printf("\nDESEA AGREGAR OTRO PRODUCTO? [s/n]: ");
-        fflush(stdin);
-        scanf("%s", reintento);
-        validarS_N( reintento );
-
-        if(strcmp( reintento, "n" ) == 0){
-            break;
-        }
-
-    }
-    printf("\n============== VOLVIENDO AL MENU ================\n");
-    system("pause");
-}
-
-void eliminarProductoCarrito(Stock* almacen, HashMap* MapCarritos){
-    
-    if( MapCarritos->size == 0 ){
-        system("cls");
-        printf("\n======================= ELIMINAR PRODUCTO DEL CARRITO ========================\n");
-        printf("\n                  ACTUALMENTE NO HAY CARRITOS DE COMPRAS\n");
-        printf("\n============================ VOLVIENDO AL MENU ===============================\n");
-        system("pause");
-        return;
-    }
-
-    char reintento[2];
-    char nombreAux[31];
-    
-    Carrito* carritoAux;
-    ProductoCarrito* pdctCarritoAux;
-    while( true ){
-        system("cls");
-        printf("\n======================= ELIMINAR PRODUCTO DEL CARRITO ========================\n");
-        printf("\nNombre del carrito: ");
-        fflush(stdin);
-        gets(nombreAux);
-        Pair* aux;
-        if( (aux = searchMap(MapCarritos, nombreAux)) ){
-            carritoAux = (Carrito*) aux->value;
-            pdctCarritoAux = (ProductoCarrito*) popBack(carritoAux->productos);
-            carritoAux->cantTotalProductos -= pdctCarritoAux->cantidad;
-            carritoAux->cantProductos -= 1; 
-            
-            if( carritoAux->cantProductos == 0){
-                printf("\nSE EXTRAJO EL ULTIMO PRODUCTO DEL CARRITO\n");
-                printf("\nELIMINANDO EL CARRITO\n");
-                eraseMap(MapCarritos, carritoAux->nombre);
-            }
-            else{
-                printf("\nSE EXTRAJO EL PRODUCTO DEL CARRITO\n");
-            }
-
-            pdctCarritoAux->referencia->stock += pdctCarritoAux->cantidad;              //Devolver la cantidad del carrito al stock
-            if( !(aux = searchMap(almacen->nombre, pdctCarritoAux->referencia->nombre)) ){      //Si el producto se habia eliminado, agregarlo denuevo
-                agregarProducto(almacen , pdctCarritoAux->referencia);
-            }
-
-        }
-        else{
-            printf("\nCARRITO NO ENCONTRADO\n");
-        }
-
-        printf("\nDESEA ELIMINAR OTRO PRODUCTO? [s/n]: ");
-        fflush(stdin);
-        scanf("%s", reintento);
-        validarS_N( reintento );
-
-        if(strcmp( reintento, "n" ) == 0){
-            break;
-        }
-
-    }
-    printf("\n============================ VOLVIENDO AL MENU ===============================\n");
-    system("pause");
-
-}
-
-void comprarCarrito( HashMap* MapCarritos ){
-    system("cls");
-    if( MapCarritos->size == 0 ){
-        printf("\n======================= EFECTUANDO COMPRA DE CARRITO ========================\n");
-        printf("\n                  ACTUALMENTE NO HAY CARRITOS DE COMPRAS\n");
-        printf("\n============================ VOLVIENDO AL MENU ==============================\n");
-        system("pause");
-        return;
-    }
-    
-    char nombreAux[31];
-    printf("\n======================= EFECTUANDO COMPRA DE CARRITO ========================\n");
-    printf("\nNombre del carrito: ");
-    fflush(stdin);
-    gets(nombreAux);
-    Pair* aux;
-    if( aux = searchMap(MapCarritos, nombreAux) ){
-        system("cls");
-        printf("\n=========================================================== BOLETA =============================================================\n");
-        printf("\n==================== PRODUCTO ========================= MARCA ============= TIPO ============ CANTIDAD ==== PRECIO ==== TOTAL ==\n");
-        int total = 0;
-        Carrito* carritoAux;
-        ProductoCarrito* pdctCarritoAux;
-        Producto* productoAux;
-        carritoAux = (Carrito*) aux->value;
-        pdctCarritoAux = popFront(carritoAux->productos);
-        while(carritoAux->cantProductos > 0){
-
-            productoAux = (Producto*) pdctCarritoAux->referencia;
-            printf("\n%50s", productoAux->nombre);
-            printf("%20s", productoAux->marca);
-            printf("%20s", productoAux->tipo);
-            printf("%12i", pdctCarritoAux->cantidad);
-            printf("%12i", productoAux->valor);
-            printf("%11i", (pdctCarritoAux->cantidad * productoAux->valor) );
-            total += (pdctCarritoAux->cantidad * productoAux->valor);
-
-            pdctCarritoAux = popFront(carritoAux->productos);
-            carritoAux->cantProductos -= 1;
-        }
-        eraseMap(MapCarritos, carritoAux->nombre);
-        printf("\n\n================================================================================================================================\n");
-        printf("\n                                                  TOTAL A PAGAR : %c%i\n", 36, total);
-        printf("\n====================================================== VOLVIENDO AL MENU =======================================================\n");
-        system("pause");
-    }
-    else{
-        printf("\nCARRITO NO ENCONTRADO\n");
-        printf("\n============================ VOLVIENDO AL MENU ==============================\n");
-        system("pause");
-    }
-}
-
-void mostrarCarritos(HashMap* MapCarritos ){
-    system("cls");
-    if( MapCarritos->size == 0 ){
-        printf("\n======================= MOSTRANDO TODOS LOS CARRITOS ========================\n");
-        printf("\n                  ACTUALMENTE NO HAY CARRITOS DE COMPRAS\n");
-        printf("\n============================ VOLVIENDO AL MENU ==============================\n");
-        system("pause");
-        return;
-    }
-    printf("\n======================= MOSTRANDO TODOS LOS CARRITOS ========================\n");
-    int cont = 1;
-    Pair* aux;
-    Carrito* carritoAux;
-    aux = firstMap(MapCarritos);
-    while(aux){
-        carritoAux = (Carrito*) aux->value;
-        printf("\nNombre carrito de compra %i: %s", cont, carritoAux->nombre);
-        printf("\nCantidad de productos agregados: %i", carritoAux->cantProductos);
-        printf("\nCantidad de productos totales que tiene:   %i\n", carritoAux->cantTotalProductos);
-        aux = nextMap(MapCarritos);
-        cont += 1;
-    }
-    printf("\n============================ VOLVIENDO AL MENU ==============================\n");
     system("pause");
 }
 
 int main() { 
+    
     system("color 7c");
     Stock *almacen = createStock();
-    HashMap* carritosDeCompras = createMap(15);
+    //Carritos* carritosDeCompras = createCarritos();
 
     int opcion = -1;
     char aux[51];
@@ -802,7 +462,7 @@ int main() {
             case 4: 
                 buscarTipo( almacen->tipo );
                 break;
-            case 5: 
+            /*case 5: 
                 buscarMarca( almacen->marca );
                 break;
             case 6: 
@@ -810,19 +470,21 @@ int main() {
                 break;
             case 7: 
                 mostrarProductos( almacen->nombre );
-                break;
-            case 8: 
-                agregarProductoCarrito( almacen, carritosDeCompras );
-                break;
-            case 9: 
+                break;*/
+            //case 8: 
+                //agregarProductoCarrito( almacen, carritosDeCompras );
+              //  break;
+            /*case 9: 
                 eliminarProductoCarrito( almacen, carritosDeCompras );
                 break;
-            case 10:
-                comprarCarrito( carritosDeCompras );
-                break;
-            case 11:
+            case 10:*/
+                //comprarCarrito( carritosDeCompras );
+                //break;
+            /*case 11:
                 mostrarCarritos( carritosDeCompras );
                 break;
+            case 12:
+                return 0;*/
         }
     }
     
